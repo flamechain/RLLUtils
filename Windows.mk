868d84a51a6182ba6df436dea80aede7609ec54d
flamechain <rwc@thezulus.com>
2022-08-18 15:39:47 -0700

    updated install directions

--- Windows.mk
+++ Windows.mk
@@ -6,10 +6,6 @@ INSTALL_DIR=
 all: init install
 
 init:
-ifneq ($(shell id -u), 0)
-	$(error cannot install without administrator permissions)
-endif
-
 ifeq ($(INSTALL_DIR),)
 	$(error no install directory provided (example: make INSTALL_DIR="C:/Program Files/RLLUtils"))
 endif
@@ -21,15 +17,15 @@ endif
 
 install:
 	@echo installing EasyATP ...
-	cp bin/easyatp.exe "$(INSTALL_DIR)/bin"
+	copy ".\\bin\\easyatp.exe" "$(INSTALL_DIR)\\bin"
 	./bin/installer.exe "$(INSTALL_DIR)/bin/easyatp.exe"
 
 	@echo done
 
 uninstall:
 
-	rm "$(INSTALL_DIR)/rllutils.temp"
+	del /f "$(INSTALL_DIR)\\rllutils.temp"
 	./bin/uninstaller.exe
-	rm -r "$(INSTALL_DIR)"
+	rmdir /s "$(INSTALL_DIR)"
 
 	@echo done
